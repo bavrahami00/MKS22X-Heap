0a648330f84398fe0d87b9e03041ece112ea98c9
bavrahami00 <bavrahami00@stuy.edu>
2019-04-17 14:00:41 -0400

    Created method names

---
+++ MyHeap.java
@@ -0,0 +1,8 @@
+public class MyHeap {
+  private static void pushDown(int[] data, int size, int index) {
+  }
+  public static void heapify(int[] data) {
+  }
+  public static void heapsort(int[] data) {
+  }
+}
