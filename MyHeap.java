48973393533c35db14b054626420aea64b4fc8b3
bavrahami00 <bavrahami00@stuy.edu>
2019-04-23 14:05:43 -0400

    Finished pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,5 +1,33 @@
 public class MyHeap {
   private static void pushDown(int[] data, int size, int index) {
+    if (size <= index*2+1) {
+      return;
+    }
+    if (size != index*2+2) {
+      if (data[index*2+1] > data[index*2+2]) {
+        if (data[index*2+1] > data[index] {
+          int temp = data[index];
+          data[index] = data[index*2+1];
+          data[index*2+1] = temp;
+          pushDown(data,size,index*2+1);
+        }
+      }
+      else {
+        if (data[index*2+2] > data[index] {
+          int temp = data[index];
+          data[index] = data[index*2+2];
+          data[index*2+2] = temp;
+          pushDown(data,size,index*2+2);
+        }
+      }
+    }
+    else {
+      if (data[index*2+1] > data[index]) {
+        int temp = data[index];
+        data[index] = data[index*2+1];
+        data[index*2+1] = temp;
+      }
+    }
   }
   public static void heapify(int[] data) {
   }
